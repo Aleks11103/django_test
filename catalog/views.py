0f91ca6b61f396e88fae587833a78ab88585428f
Aleksei Smantser <lesha_smancer@mail.ru>
2021-02-27 22:03:46 +0300

    part6 ready

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Author, Book, BookInstance, Genre
+from django.views.generic import ListView, DetailView
 
 
 def index(request):
@@ -21,3 +22,51 @@ def index(request):
             'book_i_in_title': book_i_in_title,
         }
     )
+
+
+class BookListView(ListView):
+    model = Book
+    template_name = 'book_list.html'
+    paginate_by = 10
+    # context_object_name = 'my_book_list'
+    # queryset = Book.objects.filter(title__icontains='и')[:5]
+    # template_name = 'books/my_arbitrary_template_name_list.html'
+
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='')[:5]
+
+    # def get_context_data(self, **kwargs):
+    #     context = super(BookListView, self).get_context_data(**kwargs)
+    #     context['some_data'] = 'This is just some data'
+    #     return context
+    
+    
+class BookDetailView(DetailView):
+    model = Book
+    template_name = 'book_detail.html'
+
+#Если бы ен использовался class BookDetailView
+# def book_detail_view(request,pk):
+#     try:
+#         book_id=Book.objects.get(pk=pk)
+#     except Book.DoesNotExist:
+#         raise Http404("Book does not exist")
+
+#     #book_id=get_object_or_404(Book, pk=pk)
+
+#     return render(
+#         request,
+#         'catalog/book_detail.html',
+#         context={'book':book_id,}
+#     )
+
+
+class AuthorListView(ListView):
+    model = Author
+    template_name = 'author_list.html'
+    paginate_by = 10
+
+
+class AuthorDetailView(DetailView):
+    model = Author
+    template_name = 'author_detail.html'
